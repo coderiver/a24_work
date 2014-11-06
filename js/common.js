c57ded6fedc4a904f88986b277a584c38d9ba00c
moroz <morozarturv@gmail.com>
2014-11-06 15:41:15 +0200

    main page

--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
 head.ready(function() {
 
-	
+	var search = $('.js-search'),
+			search_type = search.find('.search__type'),
+			search_title = search.find('.search__title');
+
+	// search
+	search_title.on('click', function () {
+		search_type.removeClass('is-active');
+		$(this).parent().addClass('is-active');
+	});	
 	
 });
