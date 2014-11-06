abf03d1ff2185dec07377e2b8331ac83457353b7
moroz <morozarturv@gmail.com>
2014-11-06 16:18:34 +0200

    added up button

--- js/common.js
+++ js/common.js
@@ -2,12 +2,20 @@ head.ready(function() {
 
 	var search = $('.js-search'),
 			search_type = search.find('.search__type'),
-			search_title = search.find('.search__title');
+			search_title = search.find('.search__title'),
+			up = $('.js-up');
 
 	// search
 	search_title.on('click', function () {
 		search_type.removeClass('is-active');
 		$(this).parent().addClass('is-active');
 	});	
+
+	// up
+	up.on('click', function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
+	});
 	
 });
