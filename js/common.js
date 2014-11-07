57cd8e57a786cd9d6236ee88337bf00d3f93e306
moroz <morozarturv@gmail.com>
2014-11-07 14:56:54 +0200

    main fixes

--- js/common.js
+++ js/common.js
@@ -3,7 +3,11 @@ head.ready(function() {
 	var search = $('.js-search'),
 			search_type = search.find('.search__type'),
 			search_title = search.find('.search__title'),
-			up = $('.js-up');
+			up = $('.js-up'),
+			vote = $('.js-vote'),
+			vote_in = vote.find('.vote__in'),
+			vote_result = vote.find('.vote__result'),
+			vote_btn = vote_in.find('.btn');
 
 	// search
 	search_title.on('click', function () {
@@ -16,6 +20,12 @@ head.ready(function() {
 		$('html, body').animate({
 			scrollTop: 0
 		}, 500);
+	});
+
+	// vote
+	vote_btn.on('click', function () {
+		vote_in.hide();
+		vote_result.show();
 	});
 	
 });
